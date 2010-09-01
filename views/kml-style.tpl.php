ef2d7fcdb8ab2c0ab05cd55d5b2be318eb0c1d3d
Robert Soden <robertsoden@rsoden.local>
2010-09-01 18:16:49 -0400

    first commit

---
+++ views/kml-style.tpl.php
@@ -0,0 +1,15 @@
+<?php
+// $Id: kml-style.tpl.php,v 1.1.2.1 2010/06/11 15:07:21 rsoden Exp $
+
+/*
+ * Empty placeholder for styles
+ */
+?>
+<Style id="<?php print $id ?>">
+  <IconStyle>
+    <Icon>
+      <href><?php print $iconurl ?></href>
+    </Icon>
+    <scale><?php print $scale ?></scale>
+  </IconStyle>
+</Style>
