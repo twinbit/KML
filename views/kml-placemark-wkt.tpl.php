fc207e40fd18ee28085e05f0db475befdb44dcc3
Paolo e Stefano Mainardi <info@twinbit.it>
2010-10-08 16:26:03 +0200

    new option to select additional node title field to use in description, to avoid html title escaping on google earth

--- views/kml-placemark-wkt.tpl.php
+++ views/kml-placemark-wkt.tpl.php
@@ -1,14 +1,12 @@
-<?php
-// $Id: kml-placemark.tpl.php,v 1.1.2.1 2010/06/03 14:14:09 tmcw Exp $
-?><Placemark>
+<Placemark>
   <name>
-    <![CDATA[<?php print $name ?>]]>
+    <![CDATA[<?php print strip_tags($name) ?>]]>
   </name>
   <description>
     <![CDATA[<?php print $description ?>]]>
   </description>
   <?php if ($styleURL): ?>
-  <styleUrl><?php echo $styleURL; ?></styleUrl>
+    <styleUrl><?php echo $styleURL; ?></styleUrl>
   <?php endif; ?>
   <?php print $geometry ?>
 </Placemark>
